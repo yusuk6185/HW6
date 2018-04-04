37c961e56b743dd777fe3059514f3fba0356655b
Yu Suk Choi <yuchoi@uiowa.edu>
2018-04-04 01:13:08 -0500

    troubleshoot

---
+++ src/java/dbHelpers/ReadQuery.java
@@ -0,0 +1,104 @@
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Barcasquad;
+
+public class ReadQuery {
+    
+    private Connection conn;
+    private ResultSet results;
+    
+    public ReadQuery() {
+        
+    Properties props = new Properties();
+    InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    String driver = props.getProperty("driver.name");
+    String url = props.getProperty("server.name");
+    String username = props.getProperty("user.name");
+    String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void doRead() {
+        try {
+            String query = "Select * from barcasquad";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String getHTMLTable(){
+        
+        String table = "";
+        
+        table += "<table border=1>";
+        
+        try {
+            while(this.results.next()){
+                
+                Barcasquad barca = new Barcasquad();
+                barca.setPlayerID(this.results.getInt("playerID"));
+                barca.setPlayerName(this.results.getString("playerName"));
+                barca.setPlayerPosition(this.results.getString("playerPosition"));
+                barca.setAge(this.results.getInt("age"));
+                
+                table += "<tr>";
+                table += "<td>";
+                table += barca.getPlayerID();
+                table += "</td>";
+                                
+                table += "<td>";
+                table += barca.getPlayerName();
+                table += "</td>";
+                                                
+                table += "<td>";
+                table += barca.getPlayerPosition();
+                table += "</td>";
+                                                                
+                table += "<td>";
+                table += barca.getAge();
+                table += "</td>";
+                                                                                
+                table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        table += "</table>";
+        
+            return table;
+    }
+}
--- web/read.jsp
+++ web/read.jsp
@@ -3,11 +3,13 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Barca Squad</title>
+        <title>JSP Page</title>
     </head>
+    
+    <% String table = (String) request.getAttribute("table"); %>
+    
     <body>
-        <h1>Barca Squad Database</h1>
-        
-        <a href="read">View All Squad</a>
+        <h1>Barca Squad</h1>
+        <%= table %>
     </body>
 </html>
